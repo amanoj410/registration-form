634a351a48854e2d4e49346775c03a0c5db899d8
amanoj410 <83334336+amanoj410@users.noreply.github.com>
2023-03-28 21:54:43 +0530

    Add files via upload

---
+++ 20f227/index.js
@@ -0,0 +1,15 @@
+function editAllElements(){
+    // var table=document.getElementById("table1");
+    // if($("#table tbody").length==0){
+    //     $("#table").append("<tbody></tbody>");
+    // }
+    // $("#table tbody").append("<tr>");
+    // for(var i=0;i<6;i++){
+    //     $("#table tbody").append(`<td><input type="text"></td>`);
+    // }
+    // $("#table tbody").append("</tr");
+    document.write()
+}
+function deleteAnElement(){
+
+}
